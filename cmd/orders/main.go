975ec2d85078c942b7708e47d25b26b22e5680de
dyhalmeida <dyhalmeida@gmail.com>
2025-04-13 10:07:25 -0300

    fix: update get request path for listing orders from "/orders" to "/order"

--- cmd/orders/main.go
+++ cmd/orders/main.go
@@ -46,7 +46,7 @@ func main() {
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
 	webserver.AddHandler(http.MethodPost, "/order", webOrderHandler.Create)
-	webserver.AddHandler(http.MethodGet, "/orders", webOrderHandler.List)
+	webserver.AddHandler(http.MethodGet, "/order", webOrderHandler.List)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
 
