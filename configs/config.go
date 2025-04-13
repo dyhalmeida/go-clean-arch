7bf763ef6c7682a20347a66231b1f3f77e55b93b
dyhalmeida <dyhalmeida@gmail.com>
2025-04-13 12:39:41 -0300

    feat: enhance configuration management by adding RabbitMQ settings and improving error handling

--- configs/config.go
+++ configs/config.go
@@ -12,22 +12,44 @@ type conf struct {
 	WebServerPort     string `mapstructure:"WEB_SERVER_PORT"`
 	GRPCServerPort    string `mapstructure:"GRPC_SERVER_PORT"`
 	GraphQLServerPort string `mapstructure:"GRAPHQL_SERVER_PORT"`
+	RabbitMQHost      string `mapstructure:"RABBITMQ_HOST"`
+	RabbitMQPort      string `mapstructure:"RABBITMQ_PORT"`
+	RabbitMQUser      string `mapstructure:"RABBITMQ_USER"`
+	RabbitMQPass      string `mapstructure:"RABBITMQ_PASS"`
 }
 
 func LoadConfig(path string) (*conf, error) {
 	var cfg *conf
-	viper.SetConfigName("app_config")
+
+	viper.AutomaticEnv()
+
+	viper.BindEnv("DB_DRIVER")
+	viper.BindEnv("DB_HOST")
+	viper.BindEnv("DB_PORT")
+	viper.BindEnv("DB_USER")
+	viper.BindEnv("DB_PASSWORD")
+	viper.BindEnv("DB_NAME")
+	viper.BindEnv("WEB_SERVER_PORT")
+	viper.BindEnv("GRPC_SERVER_PORT")
+	viper.BindEnv("GRAPHQL_SERVER_PORT")
+	viper.BindEnv("RABBITMQ_HOST")
+	viper.BindEnv("RABBITMQ_PORT")
+	viper.BindEnv("RABBITMQ_USER")
+	viper.BindEnv("RABBITMQ_PASS")
+
+	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
-	viper.SetConfigFile(".env")
-	viper.AutomaticEnv()
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, err
+		}
 	}
-	err = viper.Unmarshal(&cfg)
+
+	err := viper.Unmarshal(&cfg)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return cfg, err
+	return cfg, nil
 }
